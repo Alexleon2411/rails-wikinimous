2372858e23af5d709625e823a6aa047b58130219
Alejandro Leon <79841123+Alexleon2411@users.noreply.github.com>
2023-05-28 21:52:15 +0200

    updated exit .exit quit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
